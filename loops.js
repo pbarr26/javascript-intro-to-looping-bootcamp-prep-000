d8ba959cb99f7e8f78bb928396d587c981a647fa
cerebral-map-4184 <patriciagbarrett@gmail.com>
2019-01-22 05:30:29 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,7 @@
-function forLoop(array){
-for (i = 0; i < 25; i++) {
-if(i===1){
-array.push(“I am 1 strange loop.”);
-} else {
-array.push(“I am ${i} strange loop.”)
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+  if(i===1) {array.push("I am " + i + " strange loop")}
+  else {array.push("I am ${i} strange loop")
 }
-}
-return array;
+return array
 }
